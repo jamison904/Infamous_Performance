754afece55669e0bc81e69a6be2906b0174f088d
h0rn3t <horn3t69@gmail.com>
2013-12-29 18:29:02 +0200

    ...

--- src/com/brewcrewfoo/performance/activities/BuildPropEditor.java
+++ src/com/brewcrewfoo/performance/activities/BuildPropEditor.java
@@ -18,6 +18,7 @@ import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -31,6 +32,7 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.brewcrewfoo.performance.R;
@@ -84,9 +86,9 @@ public class BuildPropEditor extends Activity implements Constants, AdapterView.
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position,long id) {
                 final Prop p = adapter.getItem(position);
-                if(p.getName().contains("fingerprint")) return true;
-                makedialog(getString(R.string.del_prop_title),getString(R.string.del_prop_msg,p.getName()),(byte)1,p);
-                return false;
+                if(!p.getName().contains("fingerprint"))
+                    makedialog(getString(R.string.del_prop_title),getString(R.string.del_prop_msg,p.getName()),(byte)1,p);
+                return true;
             }
         });
         packList.setOnItemClickListener(this);
@@ -222,14 +224,17 @@ public class BuildPropEditor extends Activity implements Constants, AdapterView.
         final View editDialog = factory.inflate(R.layout.build_prop_dialog, null);
         final EditText tv = (EditText) editDialog.findViewById(R.id.vprop);
         final EditText tn = (EditText) editDialog.findViewById(R.id.nprop);
+        final TextView tt = (TextView) editDialog.findViewById(R.id.text1);
         final Spinner sp = (Spinner) editDialog.findViewById(R.id.spinner);
         final LinearLayout lpresets = (LinearLayout) editDialog.findViewById(R.id.lpresets);
         ArrayAdapter<CharSequence> vAdapter = new ArrayAdapter<CharSequence>(context, android.R.layout.simple_spinner_item);
         vAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         vAdapter.clear();
 
+
         if(pp!=null){
             final String v=pp.getVal();
+
             lpresets.setVisibility(LinearLayout.GONE);
             if(v.equals("0")){
                 vAdapter.add("0");
@@ -270,6 +275,8 @@ public class BuildPropEditor extends Activity implements Constants, AdapterView.
 
             tv.setText(pp.getVal());
             tn.setText(pp.getName());
+            tn.setVisibility(EditText.GONE);
+            tt.setText(pp.getName());
             titlu=getString(R.string.edit_prop_title);
 
         }
@@ -282,6 +289,9 @@ public class BuildPropEditor extends Activity implements Constants, AdapterView.
             vAdapter.add("false");
             sp.setAdapter(vAdapter);
             lpresets.setVisibility(LinearLayout.VISIBLE);
+            tt.setText(getString(R.string.prop_name));
+            tn.setVisibility(EditText.VISIBLE);
+
         }
         sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -308,6 +318,7 @@ public class BuildPropEditor extends Activity implements Constants, AdapterView.
                             if (tv.getText().toString() != null){
                                 pp.setVal(tv.getText().toString().trim());
                                 new CMDProcessor().su.runWaitFor(getFilesDir()+"/utils -setprop \""+pp.getName()+"="+pp.getVal()+"\"");
+                                //Log.d(TAG, "/utils -setprop \""+pp.getName()+"="+pp.getVal()+"\"");
                             }
                         }
                         else {
@@ -316,6 +327,7 @@ public class BuildPropEditor extends Activity implements Constants, AdapterView.
                                 new CMDProcessor().su.runWaitFor(getFilesDir()+"/utils -setprop \""+tn.getText().toString().trim()+"="+tv.getText().toString().trim()+"\"");
                             }
                         }
+
                         Collections.sort(props);
                         adapter.notifyDataSetChanged();
                     }
