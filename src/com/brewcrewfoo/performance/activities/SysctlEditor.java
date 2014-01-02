aef84893eb6a3506d80c725e4ace2453c5365d5a
h0rn3t <horn3t69@gmail.com>
2014-01-02 11:27:47 +0200

    ...

--- src/com/brewcrewfoo/performance/activities/SysctlEditor.java
+++ src/com/brewcrewfoo/performance/activities/SysctlEditor.java
@@ -7,7 +7,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -24,12 +23,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -64,6 +65,7 @@ public class SysctlEditor extends Activity implements Constants, AdapterView.OnI
 
     private final String syspath="/system/etc/";
     private String sob=SYSCTL_SOB;
+    private String[] tcp={};
 
 
     @Override
@@ -183,7 +185,12 @@ public class SysctlEditor extends Activity implements Constants, AdapterView.OnI
     private class GetPropOperation extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... params) {
-            CMDProcessor.CommandResult cr=new CMDProcessor().sh.runWaitFor("busybox echo `busybox find /proc/sys/* -type f -perm -644 | grep -v \"vm.\"`");
+            CMDProcessor.CommandResult cr=null;
+            cr=new CMDProcessor().sh.runWaitFor("busybox echo `sysctl -n net.ipv4.tcp_available_congestion_control`");
+            if(cr.success()){
+                tcp=cr.stdout.split(" ");
+            }
+            cr=new CMDProcessor().sh.runWaitFor("busybox echo `busybox find /proc/sys/* -type f -perm -644 | grep -v \"vm.\"`");
             if(cr.success()){
                 return cr.stdout;
             }
@@ -250,19 +257,63 @@ public class SysctlEditor extends Activity implements Constants, AdapterView.OnI
         String titlu="";
 
         LayoutInflater factory = LayoutInflater.from(this);
-        final View editDialog = factory.inflate(R.layout.prop_edit_dialog, null);
+        final View editDialog = factory.inflate(R.layout.build_prop_dialog, null);
         final EditText tv = (EditText) editDialog.findViewById(R.id.vprop);
-        final TextView tn = (TextView) editDialog.findViewById(R.id.nprop);
+        final EditText tn = (EditText) editDialog.findViewById(R.id.nprop);
+        final TextView tt = (TextView) editDialog.findViewById(R.id.text1);
+        final Spinner sp = (Spinner) editDialog.findViewById(R.id.spinner);
+        final LinearLayout lpresets = (LinearLayout) editDialog.findViewById(R.id.lpresets);
+        ArrayAdapter<CharSequence> vAdapter = new ArrayAdapter<CharSequence>(context, android.R.layout.simple_spinner_item);
+        vAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        vAdapter.clear();
 
         if(pp!=null){
+            final String v=pp.getVal();
+            final String n=pp.getName();
+            lpresets.setVisibility(LinearLayout.GONE);
+            if(n.contains("net.ipv4.tcp_congestion_control")){
+                if(tcp.length>0){
+                    vAdapter.add(v);
+                    for (String vtcp : tcp) {
+                        if(!vtcp.equals(v)) vAdapter.add(vtcp);
+                    }
+                    lpresets.setVisibility(LinearLayout.VISIBLE);
+                    sp.setAdapter(vAdapter);
+                }
+            }
+            if(v.equals("0")){
+                vAdapter.add("0");
+                vAdapter.add("1");
+                lpresets.setVisibility(LinearLayout.VISIBLE);
+                sp.setAdapter(vAdapter);
+            }
+            else if(v.equals("1")){
+                vAdapter.add("1");
+                vAdapter.add("0");
+                lpresets.setVisibility(LinearLayout.VISIBLE);
+                sp.setAdapter(vAdapter);
+            }
             tv.setText(pp.getVal());
             tn.setText(pp.getName());
+            tn.setVisibility(EditText.GONE);
+            tt.setText(pp.getName());
             titlu=getString(R.string.edit_prop_title);
         }
         else{//add
             titlu=getString(R.string.add_prop_title);
+            lpresets.setVisibility(LinearLayout.GONE);
+            tt.setText(getString(R.string.prop_name));
+            tn.setVisibility(EditText.VISIBLE);
         }
-
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                tv.setText(sp.getSelectedItem().toString().trim());
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
         new AlertDialog.Builder(this)
                 .setTitle(titlu)
                 .setView(editDialog)
