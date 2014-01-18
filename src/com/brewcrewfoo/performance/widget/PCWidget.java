b088ab9764d0e94e1db3c2c4815fd1f56b1e53a7
h0rn3t <horn3t69@gmail.com>
2014-01-18 16:00:13 +0200

    ...

--- src/com/brewcrewfoo/performance/widget/PCWidget.java
+++ src/com/brewcrewfoo/performance/widget/PCWidget.java
@@ -27,7 +27,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.RemoteViews;
 import com.brewcrewfoo.performance.R;
 import com.brewcrewfoo.performance.activities.MainActivity;
