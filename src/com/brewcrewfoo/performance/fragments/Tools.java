c423a6403837144a8f6f669dd4249daf6e2decb6
h0rn3t <horn3t69@gmail.com>
2014-01-27 23:43:30 +0200

    ...

--- src/com/brewcrewfoo/performance/fragments/Tools.java
+++ src/com/brewcrewfoo/performance/fragments/Tools.java
@@ -80,8 +80,6 @@ public class Tools extends PreferenceFragment implements OnSharedPreferenceChang
         mPreferences.registerOnSharedPreferenceChangeListener(this);
         addPreferencesFromResource(R.layout.tools);
 
-        new CMDProcessor().sh.runWaitFor("busybox mkdir -p "+dn );
-
         mResidualFiles= findPreference(RESIDUAL_FILES);
         mOptimDB= findPreference(PREF_OPTIM_DB);
 
